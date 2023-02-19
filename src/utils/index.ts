59a2aed31f69385762143baee7353e3e40dc363d
hxm <1764123064@qq.com>
2023-02-20 01:12:18 +0800

    feat: 增加常用工具函数:判断内容是否溢出、节流

---
+++ src/utils/index.ts
@@ -0,0 +1,29 @@
+// 判断是否内容溢出
+const judgeIsOverFlow = <T extends HTMLElement>(
+  target: React.MutableRefObject<T>,
+  direction: 'row' | 'col' = 'row',
+) => {
+  if (direction === 'row') {
+    return target.current.scrollWidth > target.current.clientWidth;
+  } else {
+    return target.current.scrollHeight > target.current.clientHeight;
+  }
+};
+
+// 节流
+function throttle(func: Function, delay: number) {
+  let timeout: NodeJS.Timeout | null = null; //定义一个定时器标记
+  return function (evnet: WheelEvent, ..._arguments: any) {
+    evnet.preventDefault();
+    // 判断是否存在定时器
+    if (!timeout) {
+      // 创建一个定时器
+      timeout = setTimeout(() => {
+        // delay时间间隔清空定时器
+        timeout = null;
+        func.call(this, evnet, ..._arguments);
+      }, delay);
+    }
+  };
+}
+export { judgeIsOverFlow, throttle };
