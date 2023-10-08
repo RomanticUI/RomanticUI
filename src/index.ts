f4482654c61866121f7739e949b2f1677b9d66c1
hxm <1764123064@qq.com>
2023-10-08 10:13:00 +0800

    test: 测试脚本是否执行

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,20 @@
+const updateVarLess = require('../scripts/update-css-variable')
+console.log(updateVarLess)
+updateVarLess()
+
 // 组件
-export { default as Avatar } from './components/Avatar';
-export { default as Button } from './components/Button';
-export { default as Divider } from './components/Divider';
-export * as Word from './components/Docx';
-export { default as Menu, type AllItems } from './components/Menu';
-export { default as Pagination } from './components/Pagination';
-export { default as ProgressBar } from './components/ProgressBar';
-export { default as Spin } from './components/Spin';
-export { default as SvgIcon } from './components/SvgIcon';
-export { default as Table } from './components/Table';
-export { default as Tabs } from './components/Tabs';
-export { default as Watermark } from './components/WaterMark';
+export { default as Avatar } from './components/Avatar'
+export { default as Button } from './components/Button'
+export { default as Divider } from './components/Divider'
+export * as Word from './components/Docx'
+export { default as Menu, type AllItems } from './components/Menu'
+export { default as Pagination } from './components/Pagination'
+export { default as ProgressBar } from './components/ProgressBar'
+export { default as Spin } from './components/Spin'
+export { default as SvgIcon } from './components/SvgIcon'
+export { default as Table } from './components/Table'
+export { default as Tabs } from './components/Tabs'
+export { default as Watermark } from './components/WaterMark'
 // hook
-export { default as useSummary } from './hooks/useSummary';
+export { default as useSummary } from './hooks/useSummary'
 // 对外暴露的配置
