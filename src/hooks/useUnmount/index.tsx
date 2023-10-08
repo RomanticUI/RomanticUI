c99d33305645745ae18230c0fb968b03e6fcaee0
hxm <1764123064@qq.com>
2023-10-08 11:38:08 +0800

    feat: useUnmount hook

---
+++ src/hooks/useUnmount/index.tsx
@@ -0,0 +1,9 @@
+import { useEffect } from 'react'
+import useLatest from '../useLatest'
+
+const useUnmount = (fn: () => void) => {
+  const fnRef = useLatest(fn)
+  useEffect(() => () => fnRef.current(), [])
+}
+
+export default useUnmount
